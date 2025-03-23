ec44897acf042d1d10a9b3ed35ff5538b34e0777
kvu21 <kvu21@lsu.edu>
2025-03-23 00:17:18 -0500

    Inventory and item pickup

--- LLL/Scenes/laika.gd
+++ LLL/Scenes/laika.gd
@@ -2,24 +2,22 @@ extends CharacterBody2D
 
 
 @export var speed = 400
-@onready var _animation_player = $AnimationPlayer
+@export var inv: Inv
+@onready var _animation_player = $AnimatedSprite2D
 const msScript = preload("res://Scenes/main_stage.gd") #mainstage script
 var mainstage = msScript.new()
-var weapon = 0
-var health_potion = 0
-var mana_potion = 0
 
 func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down") # uses the input map found int he project 
+	var input_direction = Input.get_vector("left", "right", "up", "down") # uses the input map found int he project 
 	velocity = input_direction * speed # Speed modifier
-	if Input.is_action_pressed("move_left"):
-		_animation_player.play("WalkingLeft")
-	elif Input.is_action_pressed("move_right"):
-		_animation_player.play("WalkingRight")
-	elif Input.is_action_pressed("move_up"):
-		_animation_player.play("WalkingUp")
-	elif Input.is_action_pressed("move_down"):
-		_animation_player.play("WalkingDown")
+	if Input.is_action_pressed("left"):
+		_animation_player.play("Left")
+	elif Input.is_action_pressed("right"):
+		_animation_player.play("Right")
+	elif Input.is_action_pressed("up"):
+		_animation_player.play("Up")
+	elif Input.is_action_pressed("down"):
+		_animation_player.play("Down")
 	else:
 		_animation_player.stop()
 	
@@ -27,12 +25,6 @@ func get_input():
 func _physics_process(delta):
 	get_input() # gets the current input
 	move_and_slide() # moves the actual icon
-	
-func add_item(item):
-	if(item == "weapon"):
-		weapon += 1
-	if(item == "health_potion"):
-		health_potion += 1
-	if(item == "mana_potion"):
-		mana_potion += 1
-	pass
+
+func collect(item):
+	inv.insert(item)
--- LLL/Scripts/inventory/Inv_UI.gd
+++ LLL/Scripts/inventory/Inv_UI.gd
@@ -1,17 +1,18 @@
 extends Control
 
-@onready var inv: Inv = preload("res://inventory/playerInv.tres")
+@onready var inv: Inv = preload("res://Scripts/inventory/playerInv.tres")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 
 var is_open = false
 
 func _ready():
+	inv.update.connect(update_slots)
 	update_slots()
 	close()
 
 func update_slots():
-	for i in range(min(inv.items.size(), slots.size())):
-		slots[i].update(inv.items[i])
+	for i in range(min(inv.slots.size(), slots.size())):
+		slots[i].update(inv.slots[i])
 
 func _process(delta):
 	if Input.is_action_just_pressed("tab"):
@@ -19,7 +20,6 @@ func _process(delta):
 			close()
 		else:
 			open()
-	
 
 func open():
 	visible = true
