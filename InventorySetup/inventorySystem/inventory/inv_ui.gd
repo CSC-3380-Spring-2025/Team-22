fc23a9807e46ace11bb2bcc903f73da9eeea23b3
Emery <ejon174@lsu.edu>
2025-03-29 08:33:02 -0500

    Fix merge conflict
    Readded my lost files and accidently deleted some in the process, I did a revert and manually went through each file.

--- InventorySetup/inventorySystem/inventory/inv_ui.gd
+++ InventorySetup/inventorySystem/inventory/inv_ui.gd
@@ -1,18 +1,17 @@
 extends Control
 
-@onready var inv: Inv = preload("res://Scripts/inventory/playerInv.tres")
+@onready var inv: Inv = preload("res://inventory/playerInv.tres")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 
 var is_open = false
 
 func _ready():
-	inv.update.connect(update_slots)
 	update_slots()
 	close()
 
 func update_slots():
-	for i in range(min(inv.slots.size(), slots.size())):
-		slots[i].update(inv.slots[i])
+	for i in range(min(inv.items.size(), slots.size())):
+		slots[i].update(inv.items[i])
 
 func _process(delta):
 	if Input.is_action_just_pressed("tab"):
@@ -20,6 +19,7 @@ func _process(delta):
 			close()
 		else:
 			open()
+	
 
 func open():
 	visible = true
