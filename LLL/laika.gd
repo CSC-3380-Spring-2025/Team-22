d0ed842c9779c5d8a9c96a18ff574ff7a569bb0c
Emery <ejon174@lsu.edu>
2025-04-19 23:48:43 -0500

    Added resources to tilemap for each level

--- LLL/laika.gd
+++ LLL/laika.gd
@@ -2,10 +2,10 @@ extends Area2D
 @export var speed = 400
 var screen_size
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$AnimatedSprite2D.z_index = 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float ) -> void:
