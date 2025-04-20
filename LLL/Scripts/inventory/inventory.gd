6553c1e61a9dee983d3db5850321540378de85f5
kvu21 <163810422+kvu21@users.noreply.github.com>
2025-04-19 21:03:55 -0500

    Add files via upload

--- LLL/Scripts/inventory/inventory.gd
+++ LLL/Scripts/inventory/inventory.gd
@@ -4,14 +4,18 @@ class_name Inv
 
 signal update
 
-@export var slots: Array[InvSlot]
+@export var slots: Array[InvSlot] = []
 
-func insert(item: InvItem):
-	var itemslots = slots.filter(func(slot): return slot.item == item)
+func insert(item: Item) -> void:
+	var itemslots: Array[InvSlot] = slots.filter(func(slot: InvSlot) -> bool:
+		return slot.item == item
+	)
 	if !itemslots.is_empty():
 		itemslots[0].amount += 1
 	else:
-		var emptyslots = slots.filter(func(slot): return slot.item == null)
+		var emptyslots: Array[InvSlot] = slots.filter(func(slot: InvSlot) -> bool:
+			return slot.item == null
+		)
 		if !emptyslots.is_empty():
 			emptyslots[0].item = item
 			emptyslots[0].amount = 1
