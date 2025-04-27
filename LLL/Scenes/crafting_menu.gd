82d9523f7715e45ad3cc9581a7fe0ab1bdad3d1e
crusewycoff1 <crusewycoff@gmail.com>
2025-04-26 22:44:29 -0500

    got the crafting menu working
    i got the crafting menu working

--- LLL/Scenes/crafting_menu.gd
+++ LLL/Scenes/crafting_menu.gd
@@ -1,18 +1,18 @@
-extends Sprite2D
+extends Control
 
 @onready var inv: Inv = preload("res://Scripts/inventory/playerInv.tres")
-@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+@onready var slots: Array[Node] = $inv_UI/NinePatchRect/GridContainer.getchildren()
 
 var havestick = false
 var haverock = false
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		if get_rect().has_point(to_local(event.position)):
+		if get_global_rect().has_point(event.position):
 			for i in range(min(inv.slots.size(), slots.size())):
-				if(slots[i] == "sticks"):
+				if(slots[i].value == "sticks"):
 					havestick = true
-				if(slots[i] == "rock"):
+				if(slots[i].value == "rock"):
 					haverock = true
 			if(havestick == true && haverock == true):
 				print("raygun acquired")
