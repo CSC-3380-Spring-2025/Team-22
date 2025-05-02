2c91d29d1c4f79d42c12695b376a76c57dc79be8
Emery <ejon174@lsu.edu>
2025-05-02 16:01:30 -0500

    Added tile map to overworld scene
    Added tile map to overworld scene, still needs some collision tweaks and need to work on resources spawning correctly

--- LLL/Scenes/overworld.gd
+++ LLL/Scenes/overworld.gd
@@ -20,28 +20,28 @@ func _ready() -> void:
 
 # Function that pulls from resource_data.gd that has all the different days resources per outline
 func spawn_resources():
-	var resource_config = load("res://Scripts/Resource_scripts/resource_data.gd").new()
+	var resource_config = preload("res://Scripts/Resource_scripts/resource_data.gd").new()
 	var level_data = resource_config.levels.get(level_name, [])
 	var tile_layer = $TileMap/tilemap
-#	var tile_size = tile_layer.tile_set.tile_size
+	var tile_size = tile_layer.tile_set.tile_size
 	
 	# Spawn resources
 	for resource in level_data.get("resources", []):
 		for tile_pos in resource.positions:
 			var instance = resource.scene.instantiate()
-			instance.z_index = 10
+			instance.z_index = 100
 # I commented out the part below that connects to the tile map since it isn't on overworld scene yet
 # So right now the resources are spawning on top of each other
-			#var world_pos = tile_layer.map_to_local(tile_pos) + Vector2(tile_size) / 2
-			#instance.position = world_pos
+			var world_pos = tile_layer.map_to_local(tile_pos) + Vector2(tile_size) / 2
+			instance.position = world_pos
 			add_child(instance)
 	
 	# Spawn aliens 1, 2, and final boss depending on level
 	for aliens in level_data.get("aliens", []):
 		for tile_pos in aliens.positions:
 			var instance = aliens.scene.instantiate()
-			instance.z_index = 20  
-			#instance.position = tile_layer.map_to_local(tile_pos) + Vector2(tile_size) / 2
+			instance.z_index = 100  
+			instance.position = tile_layer.map_to_local(tile_pos) + Vector2(tile_size) / 2
 			add_child(instance)
 
 
