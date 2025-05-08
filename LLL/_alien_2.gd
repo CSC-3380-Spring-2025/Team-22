84f685f7d25e93e03117d14abe5c246d1ad0cec3
kvu21 <kvu21@lsu.edu>
2025-05-07 23:59:32 -0500

    Added transitions to second type of alien and fixed one more audio

--- LLL/_alien_2.gd
+++ LLL/_alien_2.gd
@@ -1,6 +1,5 @@
 extends TextureButton
 
-# Changes scene to battle scene when button clicked
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Battle.tscn")
-	GlobalStats.currentlyFighting = 2;
+	GlobalStats.currentlyFighting = 2
+	Transition.start_transition("res://Scenes/Battle.tscn")
